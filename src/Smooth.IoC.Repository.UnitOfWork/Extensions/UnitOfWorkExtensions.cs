cd8e8054ecfe1661410f86e231d6f132bb929c6e
J.Winstanley <3266204+james-winstanley@users.noreply.github.com>
2022-09-27 17:23:18 +0100

    refactor!: all [unit of work] have transactions

--- src/Smooth.IoC.Repository.UnitOfWork/Extensions/UnitOfWorkExtensions.cs
+++ src/Smooth.IoC.Repository.UnitOfWork/Extensions/UnitOfWorkExtensions.cs
@@ -18,9 +18,12 @@ namespace Smooth.IoC.Repository.UnitOfWork.Extensions
             Action<IConditionalBulkSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class 
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ? 
-                uow.Connection.BulkDelete(statementOptions) : 
-                uow.Connection.BulkDelete<TEntity>(statement=>statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.BulkDelete<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.BulkDelete(statementOptions);
         }
 
         public static int BulkDelete<TEntity>(this IUnitOfWork uow, FormattableString whereClause, object parameters) where TEntity : class
@@ -38,9 +41,12 @@ namespace Smooth.IoC.Repository.UnitOfWork.Extensions
             Action<IConditionalBulkSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.BulkDeleteAsync(statementOptions) :
-                uow.Connection.BulkDeleteAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.BulkDeleteAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.BulkDeleteAsync(statementOptions);
         }
 
         public static Task<int> BulkDeleteAsync<TEntity>(this IUnitOfWork uow, FormattableString whereClause, object parameters) where TEntity : class
@@ -58,129 +64,172 @@ namespace Smooth.IoC.Repository.UnitOfWork.Extensions
             Action<IConditionalBulkSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.BulkUpdate(updateData, statementOptions) :
-                uow.Connection.BulkUpdate(updateData, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.BulkUpdate(updateData, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.BulkUpdate(updateData, statementOptions);
         }
 
         public static Task<int> BulkUpdateAsync<TEntity>(this IUnitOfWork uow, TEntity updateData,
             Action<IConditionalBulkSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.BulkUpdateAsync(updateData, statementOptions) :
-                uow.Connection.BulkUpdateAsync(updateData, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.BulkUpdateAsync(updateData, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.BulkUpdateAsync(updateData, statementOptions);
         }
 
         public static int Count<TEntity>(this IUnitOfWork uow,
             Action<IConditionalSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.Count(statementOptions) :
-                uow.Connection.Count<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.Count<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.Count(statementOptions);
         }
 
         public static Task<int> CountAsync<TEntity>(this IUnitOfWork uow,
             Action<IConditionalSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.CountAsync(statementOptions) :
-                uow.Connection.CountAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.CountAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.CountAsync(statementOptions);
         }
 
         public static bool Delete<TEntity>(this IUnitOfWork uow, TEntity entityToDelete,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.Delete(entityToDelete,statementOptions) :
-                uow.Connection.Delete(entityToDelete, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.Delete(entityToDelete, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.Delete(entityToDelete, statementOptions);
         }
 
         public static Task<bool> DeleteAsync<TEntity>(this IUnitOfWork uow, TEntity entityToDelete,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.DeleteAsync(entityToDelete,statementOptions) :
-                uow.Connection.DeleteAsync(entityToDelete,statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.DeleteAsync(entityToDelete, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.DeleteAsync(entityToDelete, statementOptions);
         }
 
         public static IEnumerable<TEntity> Find<TEntity>(this IUnitOfWork uow,
             Action<IRangedBatchSelectSqlSqlStatementOptionsOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.Find(statementOptions) :
-                uow.Connection.Find<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.Find<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.Find(statementOptions);
         }
 
         public static Task<IEnumerable<TEntity>> FindAsync<TEntity>(this IUnitOfWork uow,
             Action<IRangedBatchSelectSqlSqlStatementOptionsOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.FindAsync(statementOptions) :
-                uow.Connection.FindAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.FindAsync<TEntity>(statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.FindAsync(statementOptions);
         }
 
         public static TEntity Get<TEntity>(this IUnitOfWork uow, TEntity entityKeys,
             Action<ISelectSqlSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.Get(entityKeys,statementOptions) :
-                uow.Connection.Get(entityKeys,statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.Get(entityKeys, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.Get(entityKeys, statementOptions);
         }
 
         public static Task<TEntity> GetAsync<TEntity>(this IUnitOfWork uow, TEntity entityKeys,
             Action<ISelectSqlSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.GetAsync(entityKeys, statementOptions) :
-                uow.Connection.GetAsync<TEntity>(entityKeys, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.GetAsync(entityKeys, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.GetAsync(entityKeys, statementOptions);
         }
 
         public static void Insert<TEntity>(this IUnitOfWork uow, TEntity entityToInsert,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            if (statementOptions != null)
+
+            if (statementOptions is null)
             {
-                uow.Connection.Insert(entityToInsert, statementOptions);
-                return;
+                uow.Connection.Insert(entityToInsert, statement => statement.AttachToTransaction(uow.Transaction));
             }
-            uow.Connection.Insert(entityToInsert, statement => statement.AttachToTransaction(uow.Transaction));
+            else
+            {
+                statementOptions += x => x.AttachToTransaction(uow.Transaction);
+                uow.Connection.Insert(entityToInsert, statementOptions);
+            }
         }
 
         public static Task InsertAsync<TEntity>(this IUnitOfWork uow, TEntity entityToInsert,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ? 
-                uow.Connection.InsertAsync(entityToInsert, statementOptions) : 
-                uow.Connection.InsertAsync(entityToInsert, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.InsertAsync(entityToInsert, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.InsertAsync(entityToInsert, statementOptions);
         }
 
         public static bool Update<TEntity>(this IUnitOfWork uow, TEntity entityToUpdate,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.Update(entityToUpdate, statementOptions) :
-                uow.Connection.Update(entityToUpdate, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.Update(entityToUpdate, statement => statement.AttachToTransaction(uow.Transaction));
+            
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.Update(entityToUpdate, statementOptions);
         }
 
         public static Task<bool> UpdateAsync<TEntity>(this IUnitOfWork uow, TEntity entityToUpdate,
             Action<IStandardSqlStatementOptionsBuilder<TEntity>> statementOptions = null) where TEntity : class
         {
             DialogueHelper.SetDialogueIfNeeded<TEntity>(uow.SqlDialect);
-            return statementOptions != null ?
-                uow.Connection.UpdateAsync(entityToUpdate, statementOptions) :
-                uow.Connection.UpdateAsync(entityToUpdate, statement => statement.AttachToTransaction(uow.Transaction));
+
+            if (statementOptions is null)
+                return uow.Connection.UpdateAsync(entityToUpdate, statement => statement.AttachToTransaction(uow.Transaction));
+
+            statementOptions += x => x.AttachToTransaction(uow.Transaction);
+            return uow.Connection.UpdateAsync(entityToUpdate, statementOptions);
         }
     }
 }
